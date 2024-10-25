36049bfbe86fad4e15311c75b05908d09da1317d
Swelio <24651896+Swelio@users.noreply.github.com>
2024-10-25 14:12:10 +0200

    feat(year-2022): day 3 part 1 done

--- years/year-2022/src/days/day_03/part_01.rs
+++ years/year-2022/src/days/day_03/part_01.rs
@@ -1,9 +1,35 @@
+use std::str::FromStr;
+
 use aoc_traits::prelude::*;
 
-use crate::Year2022Solver;
+use crate::{
+    days::day_03::group::{rucksack::item::Priority, Group},
+    Year2022Solver,
+};
 
 impl Solver<Year2022, Day03, Part01> for Year2022Solver {
     fn solve<T: AsRef<str>>(&self, input: T) -> AocResult<Flag> {
-        todo!()
+        let group = Group::from_str(input.as_ref())?;
+        let total: Priority = group
+            .into_inner()
+            .into_iter()
+            .filter_map(|rucksack| {
+                rucksack
+                    .into_inner()
+                    .into_iter()
+                    .reduce(|intersection, current| intersection.intersection(&current))
+                    .map(|intersection| {
+                        let total: Priority = intersection
+                            .into_inner()
+                            .into_iter()
+                            .map(|item| item.priority())
+                            .sum();
+
+                        total
+                    })
+            })
+            .sum();
+
+        Ok(Flag::new(total.into_inner()))
     }
 }
