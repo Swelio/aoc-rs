b74ea854f1f6895aaf31586506370a08a2d094ed
Swelio <24651896+Swelio@users.noreply.github.com>
2025-05-27 06:50:48 +0000

    perf(2015-04): add rayon

--- years/year-2015/src/days/day_04/solver.rs
+++ years/year-2015/src/days/day_04/solver.rs
@@ -10,6 +10,7 @@ use aoc_core::{
     ports::Challenge,
 };
 use md5::{Digest, Md5, digest::generic_array::GenericArray};
+use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
 
 use super::Input;
 
@@ -30,24 +31,33 @@ impl Challenge<Solution<Part2>> for Input {
 }
 
 fn bruteforce(key: &str, pattern: &str) -> String {
-    let mut hasher = Md5::new();
-    let mut digest = [0u8; 16];
-    let mut hash = String::new();
+    (1u32..u32::MAX)
+        .into_par_iter()
+        .by_exponential_blocks()
+        .map_init(
+            || {
+                let hasher = Md5::new();
+                let digest = [0u8; 16];
+                let hash = String::new();
 
-    (1u64..)
-        .find(|suffix| {
-            let _ = write!(&mut hasher, "{key}{suffix}");
-            hasher.finalize_into_reset(GenericArray::from_mut_slice(&mut digest));
-            hash.clear();
-            digest
-                .iter()
-                .take((pattern.len() / 2) + 1)
-                .for_each(|byte| {
-                    let _ = write!(&mut hash, "{byte:02X}");
-                });
+                (hasher, digest, hash)
+            },
+            |(hasher, digest, hash), suffix| {
+                let _ = write!(hasher, "{key}{suffix}");
+                hasher.finalize_into_reset(GenericArray::from_mut_slice(digest));
+                hash.clear();
+                digest
+                    .iter()
+                    .take((pattern.len() / 2) + 1)
+                    .for_each(|byte| {
+                        let _ = write!(hash, "{byte:02X}");
+                    });
 
-            hash.starts_with(pattern)
-        })
+                hash.starts_with(pattern).then_some(suffix)
+            },
+        )
+        .find_first(|suffix| suffix.is_some())
+        .flatten()
         .expect("iterator is never empty")
         .to_string()
 }
